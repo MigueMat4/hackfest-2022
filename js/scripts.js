d0c07ed14a73ab4042b14872f713b9d95429e162
Miguel Matul <miguemat4@gmail.com>
2022-06-07 16:46:54 -0600

    Timeline agregado

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,33 @@
 // Scripts
 // 
 
-window.addEventListener('DOMContentLoaded', event => {
+var items = document.querySelectorAll(".timeline-date");
+
+function isItemInView(item){
+  var rect = item.getBoundingClientRect();
+  return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
+
+function callbackFunc() {
+    for (var i = 0; i < items.length; i++) {
+      if (isItemInView(items[i])) {
+        items[i].classList.add("show");
+      }
+    }
+  }
+
+  // listen for events
+  window.addEventListener("load", callbackFunc);
+  window.addEventListener("resize", callbackFunc);
+  window.addEventListener("scroll", callbackFunc);
+
+
+/*window.addEventListener('DOMContentLoaded', event => {
 
     // Navbar shrink function
     var navbarShrink = function () {
@@ -56,4 +82,4 @@ window.addEventListener('DOMContentLoaded', event => {
         elements: '#portfolio a.portfolio-box'
     });
 
-});
+});*/
